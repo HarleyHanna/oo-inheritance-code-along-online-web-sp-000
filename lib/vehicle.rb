9aa257a59b5b1b54b027e0c1d8068fd970d727f4
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-03 17:42:13 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
 class Vehicle
-
+  
+  
+  attr_accessor = []
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go
+    "vrrrrrroom!"
+  end
+  
+  def fill_up_tank
+    "filiing up!"
+  end
+  
 end
